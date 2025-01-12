a4803d30b1d51503ef147bb75219b82043c00233
Aleksei Makarov <makarovalexeynz@gmail.com>
2025-01-13 00:13:13 +0300

    first version hw16

---
+++ hw16/src/main/java/ru/makarov/repositories/BookRepository.java
@@ -0,0 +1,19 @@
+package ru.makarov.repositories;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.makarov.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @EntityGraph("book-author-genre")
+    @Override
+    Optional<Book> findById(Long id);
+
+    @EntityGraph("book-author-genre")
+    @Override
+    List<Book> findAll();
+}
